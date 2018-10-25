d8c592df1b920d5a9ea8ecf90ab32c69c48eb1d3
Ada Hutchings <adalynn.hutchings@gmail.com>
2018-10-24 19:21:52 -0700

    created list model

---
+++ src/routes/lists.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const listController = require("../controllers/listController");
+
+router.get("/lists", listController.index);
+
+module.exports = router;
+
