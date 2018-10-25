da19e3d9b7238ccde0f4b3418d8aaf0e3d4cff4e
Ada Hutchings <adalynn.hutchings@gmail.com>
2018-10-24 17:26:22 -0700

    added user model

--- spec/integration/users_spec.js
+++ spec/integration/users_spec.js
@@ -4,14 +4,65 @@ const base = "http://localhost:3000/users";
 
 describe("routes : users", () => {
 
-  describe("GET users/sign_up", () => {
+    describe("GET users/sign_up", () => {
 
-    it("should return a status code 200", (done) => {
-      request.get(`${base}/sign_up`, (err, res, body) => {
-        expect(res.statusCode).toBe(200);
-        done();
-      });
+        it("should return a status code 200", (done) => {
+            request.get(`${base}/sign_up`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("Sign up");
+                done();
+            });
+        });
     });
 
-  });
+    describe("POST /users", () => {
+
+        it("should create a new user with valid values and redirect", (done) => {
+            const options = {
+                url: base,
+                form: {
+                    email: "user@user.com",
+                    password: "iamuser",
+                    firstName: "mr",
+                    lastName: "user"
+                }
+            }
+            request.post(options, (err, res, body) => {
+                User.findOne({where: {email: "user@user.com"}})
+                .then((user) => {
+                    expect(user).not.toBeNull();
+                    expect(user.email).toBe("user@user.com");
+                    expect(user.firstName).toBe("mr");
+                    expect(user.lastName).toBe("user");
+                    expect(user.id).toBe(1);
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done();
+                });
+            });
+        });
+
+        it("should not create a new user if invalid inputs are given", (done) => {
+            request.post({
+                url: base,
+                form: {
+                    email: "hello",
+                    password: "1234567890",
+                    firstName: "first",
+                    lastName: "last"
+                }
+            }, (err, res, body) => {
+                User.findOne({where: {email: "hello"}})
+                .then((user) => {
+                    expect(user).toBeNull();
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done();
+                });
+            });
+        });
+    });
 });
